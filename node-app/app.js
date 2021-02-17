3e7c926bb792c4c2d3e83a92a3e964de408d8766
HazemBZ <hazembenbz@gmail.com>
2021-02-18 00:19:18 +0100

    cleaned up unused parts (websockts related)

--- node-app/app.js
+++ node-app/app.js
@@ -1,30 +1,6 @@
 let mqtt = require('mqtt')
-const WebSocket = require('ws')
 
 
-// ========= WEBSOCKET ============
-// websocket Server setup
-const server = new WebSocket.Server({
-  host:"0.0.0.0",
-  port:8000
-})
-
-let sockets = []
-server.on('connection', (socket) => {
-  sockets.push(socket)
-
-  socket.on('message', (msg)=>{ // differentitate between messages HERE
-    if (msg == "OPEN"){console.log("I HAVE TO OPEN");client.publish("command/123", "OPEN");}
-    if (msg == "CLOSE"){console.log("I HAVE TO CLOSE");client.publish("command/123", "CLOSE");}
-    sockets.forEach( s => s.send(msg));
-    console.log("[Socket Server Received] " + msg);
-  })
-  socket.on('close', ()=>{
-    sockets.filter( s => s !== socket)
-  })
-})
-
-// =================================
 
 
 // ========= MQTT ==============
@@ -61,19 +37,7 @@ client.on('error', (err)=>{
 })
 
 
-// let delay = 5000 // 5s
-// setInterval(()=>{client.publish(/*...*/)}, delay)
 
-// PUBLISH
-console.log("publishing");
-function publish(topic, message) {
-  if(client.connected == true) { // check if connected
-    client.publish(topic, message)
-    console.log("published message");
-  } else {
-    console.log("NOT CONNECTED");
-  }
-}
 
 // let timer = setInterval(()=>{publish("advertise","9999");publish("state/9999", "AOK")}, 3000)
 
@@ -81,20 +45,13 @@ function publish(topic, message) {
 // SUBSCRIBE
 console.log("subscribing");
 let subTopic=""
-let subTopics = ["advertise", "state/123", "command/123", "temp/123"]
+let subTopics = ["gaz", "flame", "temp"]
 client.subscribe(subTopics, {qos:1})
 
 // RECEIVE
 console.log("receiving");
 client.on('message', (topic, message, packet) => {
   console.log(`[received] Topic: ${topic}, Message:${message}, Packet:${packet}`);
-  console.log("|rerouting to sockets|");
- // sockets.forEach( s => s.send(`|From Mqtt Server| [Topic]:${topic}, \n[Message]:${message}`))
-  sockets.forEach( s => s.send(JSON.stringify({topic: topic, data:String(message)})))
 })
 
-
-
-// terminate connection
-// client.end()
 
